1f6c73b806f84a9cf6995296e0a62fc9f20aed4d
aron <aronphong@gmail.com>
2020-03-17 06:52:40 -0700

    Create SideDrawer component toggle

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,28 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import styles from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-    
+
+    let attachedClasses = [styles.SideDrawer, styles.Closed];
+
+    if (props.open) {
+        attachedClasses = [styles.SideDrawer, styles.Open];
+    }
+
     return (
-        <div className={styles.SideDrawer}>
-            <div className={styles.Logo}>
-                <Logo />
+        <React.Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={styles.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </React.Fragment>
     );
 };
 
